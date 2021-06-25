8d887872c20c1e49865554e664b7ac90fef0758f
Loic Chambost <chambost.loic@gmail.com>
2021-06-25 11:42:49 +0200

    ready deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { LOCALE_ID } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
-import { AppModule } from './app/app.module'
+if (!/localhost/.test(document.location.host)) {
+	enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  providers: [{provide: LOCALE_ID, useValue: 'fr-FR' }]
-}).catch(err => console.log(err))
+platformBrowserDynamic().bootstrapModule(AppModule)
+	.catch(err => console.log(err));
